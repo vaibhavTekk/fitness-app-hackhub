505e6e821c217df50b54d0d3f1d024078d10f8dc
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2022-03-05 11:13:31 +0530

    created js files

---
+++ js/data.js
@@ -0,0 +1,11 @@
+//example data , more data to be added
+//lets have 10 exp , and depending on the ones digit of the date , we choose from list
+// eg 24th march means , 4th index in list
+
+const data = [
+    {
+        exercise : "do pushups for 3 minutes",
+        description : "A push-up is a common calisthenics exercise beginning from the prone position.",
+        duration : "03:00"
+    }
+]
