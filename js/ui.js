26c613b1af972b9fb33e05f353a38b8df7964078
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2022-03-05 18:11:04 +0530

    completed logic

--- js/ui.js
+++ js/ui.js
@@ -1,29 +1,5 @@
 let array = [];
 
-window.onload = function() {
-    const dateobject = new Date()
-    var day = parseInt(dateobject.getDate().toString().slice(-1));
-    document.getElementById("exercise-image").src = data[day].location;
-    document.getElementById("instruction").innerHTML = data[day].exercise;
-    document.getElementById("description").innerHTML = data[day].description;
-    document.getElementById('timer').innerText = data[day].duration;
-
-    if (localStorage.getItem('array')){
-        array = JSON.parse(localStorage.getItem('array'));
-        const todayDate = new Date();
-        console.log(array);
-        if (array[-1] == todayDate.toDateString()) {
-            console.log("Already done for today");
-            displayScore();
-        }
-    } else {
-        const newarray = []
-        localStorage.setItem('array' , JSON.stringify(newarray))
-        array = JSON.parse(localStorage.getItem('array'));   
-    }    
-
-}
-
 const showButton = () =>{
     const button = document.getElementById("done-button");
     button.classList.remove("hidden");
@@ -33,7 +9,7 @@ const showButton = () =>{
 const submit = () => {
     const dateobject = new Date();
     var date = dateobject.toDateString();
-    array += date;
+    array.push(date);
     localStorage.setItem('array',JSON.stringify(array));
     displayScore();
 };
@@ -41,3 +17,33 @@ const submit = () => {
 const displayScore = () => {
     document.getElementById("popup").classList.remove("hidden");
 };
+
+window.onload = function() {
+    const dateobject = new Date()
+    var day = parseInt(dateobject.getDate().toString().slice(-1));
+    document.getElementById("exercise-image").src = data[day].location;
+    document.getElementById("instruction").innerHTML = data[day].exercise;
+    document.getElementById("description").innerHTML = data[day].description;
+    document.getElementById('timer').innerText = data[day].duration;
+    document.getElementById('today').innerText = `${dateobject.getDate()} / ${dateobject.getMonth() + 1} / ${dateobject.getFullYear()}`
+    
+}
+
+if (localStorage.getItem('array')){
+    array = JSON.parse(localStorage.getItem('array'));
+    const todayDate = new Date();
+    console.log(array);
+    if (array[-1] == todayDate.toDateString()) {
+        console.log();
+    } else {
+        console.log("Already done for today");
+        displayScore();
+    }
+} else {
+    const newarray = ["first item"]
+    localStorage.setItem('array' , JSON.stringify(newarray))
+    array = JSON.parse(localStorage.getItem('array'));   
+}    
+
+
+
